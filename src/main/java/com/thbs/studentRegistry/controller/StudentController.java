055956039ceca403cc2f2c9cf3c2c9be5b4e3647
jaya_muthukrishnan <jaya_muthukrishnan>
2022-03-09 03:55:08 -0800

    base url changes in swagger.yml

--- src/main/java/com/thbs/studentRegistry/controller/StudentController.java
+++ src/main/java/com/thbs/studentRegistry/controller/StudentController.java
@@ -1,11 +1,8 @@
 package com.thbs.studentRegistry.controller;
-
-
 import com.thbs.studentRegistry.entity.Student;
 import com.thbs.studentRegistry.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
